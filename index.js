5b800c7c6251892352c01426caa2aa26440934b6
Bohdan Koldun <38987744+bohdan-koldun@users.noreply.github.com>
2018-07-17 22:20:06 +0300

    add screens

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 let app = require('express')();
 let server = require('http').createServer(app);
-let io = require('socket.io')(server);
+let bodyParser = require('body-parser');
+
 let port = 3000;
 
 
 let messages = [], users = [];
 
 // Routing
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded());
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
@@ -19,63 +24,27 @@ app.get('/public/script.js', (req, res) => {
   res.sendFile(__dirname + '/public/script.js');
 });
 
+app.get('/messages', (req, res) => {
+  res.json(messages);
+});
 
-// a user conection event
-io.on('connection', function (socket) {
+app.post('/messages', (req, res) => {
+  saveNewMessage(req.body);
+});
 
-  let connectedUser = {};
+app.get('/users', (req, res) => {
+  res.json(users);
+});
 
-  //a user connected to the Chat
-  socket.on('connect to the chat', (user) => {
-    connectedUser = user;
-    socket.emit('chat history', messages);
-    socket.emit('user list', users);
-    console.log(`User ${connectedUser.nickname} connected`);
-  });
-
-  // a user is typing event
-  socket.on('user is typing', (nickname) => {
-    io.emit('user is typing', nickname);
-  });
-
-  // a user send message event
-  socket.on('chat message', (msg) => {
-    saveNewMessage(msg, messages);
-    io.emit('chat message', msg);
-  });
-
-  //add new user event
-  socket.on('new user', (user) => {
-    addNewUser(socket, user, connectedUser);
-    io.emit('user added', user);
-    io.emit('new user', user);
-     //wait 60 second and set online
-    setTimeout(() => { io.emit('user online', user); changeStatusUser('online', user.nickname) }, 60000);
-  });
-
-  // when a user disconnects
-  socket.on('disconnect', () => {
-    let userLeftMsg = {
-      name: connectedUser.name,
-      nickname: connectedUser.nickname,
-      message: '<b>@' + connectedUser.nickname + '</b> left the chat',
-      time: new Date().toUTCString()
-    };
-
-    if (connectedUser.nickname != null) {
-      saveNewMessage(userLeftMsg, messages);
-      io.emit('chat message', userLeftMsg);
-      io.emit('user just left', connectedUser); changeStatusUser('just left', connectedUser.nickname);
-      //wait 60 second and set offline
-      setTimeout(() => { io.emit('user offline', connectedUser); changeStatusUser('offline', connectedUser.nickname); }, 60000);
-    }
-  });
-
+app.post('/users', (req, res) => {
+  addNewUser(req.body);
 });
 
 
+
+
 //save new message in the history function
-function saveNewMessage(msg, messages) {
+function saveNewMessage(msg) {
   if (msg.message !== '') {
     messages.push(msg);
   }
@@ -86,31 +55,8 @@ function saveNewMessage(msg, messages) {
 }
 
 // add new user function
-function addNewUser(socket, user, connectedUser) {
-  users.forEach(item => {
-    if (item.nickname === user.nickname) {
-      user.nickname += Math.floor(Math.random() * 100);
-      socket.emit('change nickname', user.nickname);
-      connectedUser = user;
-    }
-  });
+function addNewUser(user) {
   users.push(user);
-}
-
-//change user status function
-function changeStatusUser(status, nickname) {
-  users.forEach(item => {
-    if (item.nickname === nickname) {
-      item.status = status;
-      if (status == 'online') {
-        item.color = '#00b75d';
-      } else if (status == 'just left') {
-        item.color = 'red';
-      } else if (status == 'offline') {
-        item.color = '#2b1111';
-      }
-    }
-  });
 }
 
 // conect to server
--- public/script.js
+++ public/script.js
@@ -8,34 +8,30 @@
   let userDiv = document.getElementById('users');
   let loginArea = document.getElementsByClassName('login')[0];
   let chatArea = document.getElementsByClassName('chat')[0];
-  let typingDiv = document.getElementById('is-typing-block');
 
   let userName = 'User Name';
   let userNickname = 'nickname';
 
-  let socket = io.connect();
-
   //login in the Chat
   loginButton.onclick = () => {
     userName = nameInput.value || 'User Name';
-    userNickname = nicknameInput.value || 'nickname';
+    userNickname = nicknameInput.value || 'nickname' + Math.floor(Math.random() * 100);
     loginArea.style.display = 'none';
     chatArea.style.display = 'block';
 
     let user = {
       name: userName,
-      nickname: userNickname,
-      status: 'just appeared',
-      color: '#ff9800'
+      nickname: userNickname
     };
 
-    socket.emit('new user', user);
+    ajaxRequest({
+      method: 'POST',
+      url: '/users',
+      data: user
+    });
+
+    setInterval(function () { getData(); }, 1000);
   };
-
-  socket.on('user added', (user) => {
-    socket.emit('connect to the chat', user);
-  });
-
 
   //send message to the Chat
   sendMsgButton.onclick = () => {
@@ -47,86 +43,78 @@
     };
 
     // send new chat message event to the server
-    socket.emit('chat message', data);
+    ajaxRequest({
+      method: 'POST',
+      url: '/messages',
+      data: data
+    });
+
     inputMessage.value = '';
   };
 
 
-  //get chat history event
-  socket.on('chat history', (msgArr) => {
-    messagesDiv.innerHTML = '';
-    for (let i in msgArr) {
-      if (msgArr.hasOwnProperty(i)) {
-        displayOneMsg(msgArr[i], userNickname);
+
+  let ajaxRequest = (options) => {
+    let url = options.url || '/';
+    let method = options.method || 'GET';
+    let callback = options.callback || function () { };
+    let data = options.data || {};
+    let xmlHttp = new XMLHttpRequest();
+
+    xmlHttp.open(method, url, true);
+    xmlHttp.setRequestHeader('Content-Type', 'application/json');
+    xmlHttp.send(JSON.stringify(data));
+
+    xmlHttp.onreadystatechange = () => {
+      if (xmlHttp.status == 200 && xmlHttp.readyState === 4) {
+        callback(xmlHttp.responseText);
       }
-    }
-  });
+    };
 
-  //get user list event
-  socket.on('user list', (userArr) => {
-    userDiv.innerHTML = '';
-    for (let i in userArr) {
-      if (userArr.hasOwnProperty(i)) {
-        displayOneUser(userArr[i]);
-      }
-    }
-  });
-
-
-  //new message event
-  socket.on('chat message', (msg) => {
-    displayOneMsg(msg, userNickname);
-  });
-
-  //user stats events
-  socket.on('new user', (user) => {
-    displayOneUser(user);
-  });
-
-  socket.on('user online', (user) => {
-    changeUserStatus(user, 'online')
-  });
-
-  socket.on('user just left', (user) => {
-    changeUserStatus(user, 'just left')
-  });
-
-  socket.on('user offline', (user) => {
-    changeUserStatus(user, 'offline')
-  });
-
-
-  //change duplicate nickname
-  socket.on('change nickname', (nickname) => {
-    userNickname = nickname;
-
-  });
-
-  // user is typing event
-  inputMessage.onkeyup = function () {
-    socket.emit('user is typing', userNickname);
   };
 
-  socket.on('user is typing', (nickname) => {
-    if (nickname !== userNickname) {
-      let spanNickname = document.getElementById(nickname);
 
-      if (spanNickname == null) {
-        let spanNickname = document.createElement('span');
-        spanNickname.id = nickname;
-        spanNickname.innerHTML = `@${nickname} is typing...&nbsp;`;
-        typingDiv.appendChild(spanNickname);
-
-        setTimeout(function () { spanNickname.style.display = 'none'; }, 3000);
+  let getUsers = () => {
+    ajaxRequest({
+      url: '/users',
+      method: 'GET',
+      callback: (users) => {
+        users = JSON.parse(users);
+        userDiv.innerHTML = '';
+        for (var i in users) {
+          if (users.hasOwnProperty(i)) {
+            displayOneUser(users[i]);
+          }
+        }
       }
-      else {
-        spanNickname.style.display = 'block';
+    });
 
-        setTimeout(function () { spanNickname.style.display = 'none'; }, 3000);
+  };
+
+
+  let getMessages = () => {
+    ajaxRequest({
+      url: '/messages',
+      method: 'GET',
+      callback: (msg) => {
+        msg = JSON.parse(msg);
+        messages.innerHTML = '';
+        for (var i in msg) {
+          if (msg.hasOwnProperty(i)) {
+            displayOneMsg(msg[i], userNickname);
+          }
+        }
       }
+    });
 
-    }
-  });
+  };
+
+  let getData = () => {
+    getMessages();
+    getUsers();
+  };
+
+
 
   //display Message
   function displayOneMsg(msg, nickname) {
@@ -144,31 +132,10 @@
   //display User
   function displayOneUser(user) {
     let elem = document.createElement('div');
-    elem.innerHTML = `<li><i class="fa fa-circle" id="icon-${user.nickname}"></i>${user.name} <br><b>@${user.nickname}</b><br><span class="status" id="status-${user.nickname}">${user.status}</span></li>`;
+    elem.innerHTML = `<li><i class="fa fa-user"></i>${user.name} <br><b>@${user.nickname}</b></li>`;
     userDiv.appendChild(elem);
-    document.getElementById(`icon-${user.nickname}`).style.color = user.color;
-  }
-
-  //change status
-  function changeUserStatus(user, newstatus) {
-
-    let statusText = document.getElementById(`status-${user.nickname}`);
-    let statusIcon = document.getElementById(`icon-${user.nickname}`);
-
-    if (statusText != null) {
-      statusText.innerHTML = newstatus;
-
-      if (newstatus == 'online') {
-        statusIcon.style.color = '#00b75d';
-      } else if (newstatus == 'just left') {
-        statusIcon.style.color = 'red';
-      } else if (newstatus == 'offline') {
-        statusIcon.style.color = '#2b1111';
-      }
-    }
 
   }
-
 
 
 })();
